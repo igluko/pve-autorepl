666f816f0f24b05eaa891a999319d33a53025b7b
root <root@AX101-Falkenstein-01.baikal>
2021-11-24 00:54:02 +0300

    fix cron shedule

--- cron-install.sh
+++ cron-install.sh
@@ -2,7 +2,7 @@ realpath=`realpath $0`
 dirpath=`dirname $realpath`
 scriptpath="$dirpath/pve-autorepl.py --maxvmid 499 --interval */1 --rate 50 | logger -t pve-autorepl"
 echo "script path is: $scriptpath"
-cronrecord="* * * * * $scriptpath"
+cronrecord="0 * * * * $scriptpath"
 echo "cron record is: $cronrecord"
 
 # write out current crontab
